e60facb9a03f36d40a5e74781dc3272ccf71b684
nafees nazik <nafeesnazik10@gmail.com>
2022-03-24 00:09:32 +0530

    feat(react): add spinner component

---
+++ packages/react/src/Spinner/Spinner.stories.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Spinner } from './Spinner';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+export default {
+  title: 'Components/Feedback/Spinner',
+  component: Spinner,
+  parameters: { layout: 'centered' },
+  decorators: [
+    (Story) => <div className="flex justify-center w-[90vw]">{Story()}</div>
+  ],
+  argTypes: {
+    color: {
+      control: { type: 'select' },
+      options: [
+        'primary',
+        'neutral',
+        'success',
+        'info',
+        'warning',
+        'danger',
+        'white',
+        'black',
+        'hiContrast',
+        'loContrast'
+      ]
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['xs', 'sm', 'md', 'lg', 'xl']
+    }
+  }
+} as ComponentMeta<typeof Spinner>;
+
+const Template: ComponentStory<typeof Spinner> = (args) => (
+  <Spinner {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const Sizes = () => (
+  <div className="flex items-center gap-2 flex-wrap">
+    <Spinner size="xs" />
+    <Spinner size="sm" />
+    <Spinner size="md" />
+    <Spinner size="lg" />
+    <Spinner size="xl" />
+  </div>
+);
+
+export const Colors = () => (
+  <div className="flex items-center gap-2 flex-wrap">
+    <Spinner color="primary" />
+    <Spinner color="neutral" />
+    <Spinner color="success" />
+    <Spinner color="hiContrast" />
+    <Spinner color="loContrast" />
+    <Spinner color="info" />
+    <Spinner color="warning" />
+    <Spinner color="danger" />
+    <Spinner color="white" />
+    <Spinner color="black" />
+  </div>
+);
---
+++ packages/react/src/Spinner/Spinner.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import type { GetComponentProps } from '../utils/types';
+import { styled } from 'react-cva';
+
+type SpinnerBase = GetComponentProps<typeof SpinnerBase>;
+
+const SpinnerBase = styled('svg')('animate-spin motion-reduce:hidden h-5 w-5', {
+  variants: {
+    color: {
+      primary: 'text-primary-11',
+      info: 'text-info-11',
+      success: 'text-success-11',
+      warning: 'text-warning-11',
+      danger: 'text-danger-11',
+      neutral: 'text-neutral-11',
+      white: 'text-whiteAlpha-11',
+      black: 'text-blackAlpha-11',
+      hiContrast: 'text-hiContrast',
+      loContrast: 'text-loContrast'
+    },
+    size: {
+      xs: 'h-3 w-3',
+      sm: 'h-4 w-4',
+      md: 'h-6 w-6',
+      lg: 'h-8 w-8',
+      xl: 'h-12 w-12'
+    }
+  },
+  defaultVariants: {
+    color: 'hiContrast',
+    size: 'md'
+  }
+});
+
+export type SpinnerProps = Omit<SpinnerBase, 'ref'>;
+
+export const Spinner = (props: SpinnerProps) => {
+  return (
+    <SpinnerBase
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-hidden
+      {...props}
+    >
+      <circle
+        className="opacity-25"
+        cx={12}
+        cy={12}
+        r={10}
+        stroke="currentColor"
+        strokeWidth={4}
+      />
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </SpinnerBase>
+  );
+};
