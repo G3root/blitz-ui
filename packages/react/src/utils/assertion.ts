f7b47725cecefb7681454b1c3a350730d0f688e2
nafees nazik <nafeesnazik10@gmail.com>
2022-04-05 22:30:01 +0530

    feat(utils): add isFunction util

--- packages/react/src/utils/assertion.ts
+++ packages/react/src/utils/assertion.ts
@@ -1,1 +1,8 @@
 export const __DEV__ = process.env.NODE_ENV !== 'production';
+
+// Function assertions
+export function isFunction<T extends Function = Function>(
+  value: any
+): value is T {
+  return typeof value === 'function';
+}
