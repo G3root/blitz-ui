de660d55e7aa0daf1cbc581896e7b363c7d7b6ff
nafees nazik <nafeesnazik10@gmail.com>
2022-04-05 22:31:59 +0530

    feat(utils): add more types

--- packages/react/src/utils/types.ts
+++ packages/react/src/utils/types.ts
@@ -5,3 +5,13 @@ export type GetComponentProps<T> = T extends
   | React.Component<infer P>
   ? P
   : never;
+
+export type Booleanish = boolean | 'true' | 'false';
+
+export type Merge<T, P> = P & Omit<T, keyof P>;
+
+export type FunctionArguments<T extends Function> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never;
