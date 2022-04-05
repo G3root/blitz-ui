75964002f082b7551710eac3caf3673ed100f63f
nafees nazik <nafeesnazik10@gmail.com>
2022-04-05 22:31:33 +0530

    feat(utils): add Attr utils

--- packages/react/src/utils/dom.ts
+++ packages/react/src/utils/dom.ts
@@ -1,2 +1,10 @@
+import { Booleanish } from './types';
+
 export const cx = (...classNames: any[]) =>
   classNames.filter(Boolean).join(' ');
+
+export const dataAttr = (condition: boolean | undefined) =>
+  (condition ? '' : undefined) as Booleanish;
+
+export const ariaAttr = (condition: boolean | undefined) =>
+  condition ? true : undefined;
