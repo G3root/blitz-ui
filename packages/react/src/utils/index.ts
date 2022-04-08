1ec0db49e3ff3ef5e0a337d8c28a89b3f6441ab0
nafees nazik <nafeesnazik10@gmail.com>
2022-04-08 23:29:18 +0530

    feat(utils): add utility helpers

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -2,3 +2,5 @@ export * from './types';
 export * from './assertion';
 export * from './dom';
 export * from './function';
+export * from './misc';
+export * from './system';
--- packages/react/src/utils/types.ts
+++ packages/react/src/utils/types.ts
@@ -1,8 +1,16 @@
-import * as React from 'react';
+import type {
+  ComponentType,
+  Component as ReactComponent,
+  ElementType,
+  ReactNode,
+  HTMLAttributes,
+  RefAttributes,
+  ComponentPropsWithRef
+} from 'react';
 
 export type GetComponentProps<T> = T extends
-  | React.ComponentType<infer P>
-  | React.Component<infer P>
+  | ComponentType<infer P>
+  | ReactComponent<infer P>
   ? P
   : never;
 
@@ -15,3 +23,99 @@ export type FunctionArguments<T extends Function> = T extends (
 ) => any
   ? R
   : never;
+
+/**
+ * Any object.
+ */
+export type AnyObject = Record<keyof any, any>;
+
+/**
+ * Any function.
+ */
+export type AnyFunction = (...args: any) => any;
+
+/**
+ * Render prop type.
+ * @template P Props
+ * @example
+ * const children: RenderProp = (props) => <div {...props} />;
+ */
+export type RenderProp<P = AnyObject> = (props: P) => JSX.Element | null;
+
+/**
+ * The `as` prop.
+ * @template P Props
+ */
+export type As<P = any> = ElementType<P>;
+
+/**
+ * The `wrapElement` prop.
+ */
+export type WrapElement = (element: JSX.Element | null) => JSX.Element | null;
+
+/**
+ * The `children` prop that supports a function.
+ * @template T Element type.
+ */
+export type Children<T = any> =
+  | ReactNode
+  | RenderProp<HTMLAttributes<T> & RefAttributes<T>>;
+
+/**
+ * Props with the `as` prop.
+ * @template T The `as` prop
+ * @example
+ * type ButtonOptions = Options<"button">;
+ */
+export type Options<T extends As = any> = { as?: T };
+
+/**
+ * Props that automatically includes HTML props based on the `as` prop.
+ * @template O Options
+ * @example
+ * type ButtonHTMLProps = HTMLProps<Options<"button">>;
+ */
+export type HTMLProps<O extends Options> = {
+  wrapElement?: WrapElement;
+  children?: Children;
+  [index: `data-${string}`]: unknown;
+} & Omit<ComponentPropsWithRef<NonNullable<O['as']>>, keyof O>;
+
+/**
+ * Options & HTMLProps
+ * @template O Options
+ * @example
+ * type ButtonProps = Props<Options<"button">>;
+ */
+export type Props<O extends Options> = O & HTMLProps<O>;
+
+/**
+ * A component that supports the `as` prop and the `children` prop as a
+ * function.
+ * @template O Options
+ * @example
+ * type ButtonComponent = Component<Options<"button">>;
+ */
+export type Component<O extends Options> = {
+  <T extends As>(
+    props: Omit<O, 'as'> &
+      Omit<HTMLProps<Options<T>>, keyof O> &
+      Required<Options<T>>
+  ): JSX.Element | null;
+  (props: Props<O>): JSX.Element | null;
+  displayName?: string;
+};
+
+/**
+ * A component hook that supports the `as` prop and the `children` prop as a
+ * function.
+ * @template O Options
+ * @example
+ * type ButtonHook = Hook<Options<"button">>;
+ */
+export type Hook<O extends Options> = {
+  <T extends As = NonNullable<O['as']>>(
+    props?: Omit<O, 'as'> & Omit<HTMLProps<Options<T>>, keyof O> & Options<T>
+  ): HTMLProps<Options<T>>;
+  displayName?: string;
+};
