183c5508fddaabefed7fe8d786a3160cdae24dc1
nafees nazik <nafeesnazik10@gmail.com>
2022-04-11 08:34:45 +0530

    refactor(docs): refactor ComponentContainer

--- apps/docs/src/components/component-container.tsx
+++ apps/docs/src/components/component-container.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 
 export interface ComponentContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function ComponentContainer({ children }: ComponentContainerProps) {
+export function ComponentContainer({
+  children,
+  className
+}: ComponentContainerProps) {
   return (
-    <div className="p-4 border rounded-xl mt-6 mb-4 border-neutral-4">
+    <div
+      className={`p-4 border rounded-xl mt-6 mb-4 border-neutral-4 ${className}`}
+    >
       {children}
     </div>
   );
