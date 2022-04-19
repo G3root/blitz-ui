ad65e6873ba3db0fac590314741c2a7ad1aa3b9d
nafees nazik <nafeesnazik10@gmail.com>
2022-04-19 08:29:53 +0530

    refactor(utils): use react 18 useid

--- packages/react/src/hooks/use-id.ts
+++ packages/react/src/hooks/use-id.ts
@@ -1,88 +1,35 @@
-// This implementation is heavily inspired by react-aria's implementation
+/* eslint-disable react-hooks/rules-of-hooks */
+// https://github.com/ariakit/ariakit/blob/main/packages/ariakit-utils/src/hooks.ts
 
-import * as React from 'react';
-import { __DEV__ } from '../utils';
-
-type IdContextValue = {
-  current: number;
-};
-
-const defaultIdContext: IdContextValue = {
-  current: 1
-};
-
-const IdContext = React.createContext<IdContextValue>(defaultIdContext);
-
-export const IdProvider: React.FC = React.memo(({ children }) => {
-  return React.createElement(
-    IdContext.Provider,
-    { value: { current: 1 } },
-    children
-  );
-});
-
-if (__DEV__) {
-  IdProvider.displayName = 'IdProvider';
-}
-
-const genId = (context: IdContextValue) => (context.current += 1);
-
-export function useId(idProp?: string, prefix?: string): string {
-  const context = React.useContext(IdContext);
-  /*
-      We get the current id by context and generate a new id inside useEffect so that the side effects occur during the commit phase,
-      Doing this prevents the side effects from being called twice when used with strict mode (render() in function component is the function body), which ends up making the server with the client not synchronized
-  */
-  const [id, setId] = React.useState(context.current);
-
-  React.useEffect(() => {
-    setId(genId(context));
-  }, [context]);
-
-  return React.useMemo(
-    () => idProp || [prefix, id].filter(Boolean).join('-'),
-    [idProp, prefix, id]
-  );
-}
+import {
+  useId as _useReactId,
+  useEffect,
+  useLayoutEffect,
+  useState
+} from 'react';
+import { __DEV__, canUseDOM } from '../utils';
 
 /**
- * Reack hook to generate ids for use in compound components
- *
- * @param idProp the external id passed from the user
- * @param prefixes array of prefixes to use
- *
- * @example
- *
- * ```js
- * const [buttonId, menuId] = useIds("52", "button", "menu")
- *
- * // buttonId will be `button-52`
- * // menuId will be `menu-52`
- * ```
+ * `React.useLayoutEffect` that fallbacks to `React.useEffect` on server side.
  */
-export function useIds(idProp?: string, ...prefixes: string[]) {
-  const id = useId(idProp);
-  return React.useMemo(() => {
-    return prefixes.map((prefix) => `${prefix}-${id}`);
-  }, [id, prefixes]);
-}
+export const useSafeLayoutEffect = canUseDOM ? useLayoutEffect : useEffect;
+
+const useReactId = typeof _useReactId === 'function' ? _useReactId : undefined;
 
 /**
- * Used to generate an id, and after render, check if that id is rendered so we know
- * if we can use it in places such as `aria-labelledby`.
- *
- * @param partId - The unique id for the component part
- *
- * @example
- * const { ref, id } = useOptionalPart<HTMLInputElement>(`${id}-label`)
+ * Generates a unique ID. Uses React's useId if available.
  */
-export function useOptionalPart<T = any>(partId: string) {
-  const [id, setId] = React.useState<string | null>(null);
-  const ref = React.useCallback(
-    (node: T) => {
-      setId(node ? partId : null);
-    },
-    [partId]
-  );
-  return { ref, id, isRendered: Boolean(id) };
+export function useId(defaultId?: string): string | undefined {
+  if (useReactId) {
+    const reactId = useReactId();
+    if (defaultId) return defaultId;
+    return reactId;
+  }
+  const [id, setId] = useState(defaultId);
+  useSafeLayoutEffect(() => {
+    if (defaultId || id) return;
+    const random = Math.random().toString(36).substr(2, 6);
+    setId(`id-${random}`);
+  }, [defaultId, id]);
+  return defaultId || id;
 }
--- packages/react/src/utils/dom.ts
+++ packages/react/src/utils/dom.ts
@@ -8,3 +8,16 @@ export const dataAttr = (condition: boolean | undefined) =>
 
 export const ariaAttr = (condition: boolean | undefined) =>
   condition ? true : undefined;
+
+/**
+ * It's `true` if it is running in a browser environment or `false` if it is not
+ * (SSR).
+ * @example
+ * const title = canUseDOM ? document.title : "";
+ */
+export const canUseDOM = checkIsBrowser();
+
+// Check if we can use the DOM. Useful for SSR purposes
+function checkIsBrowser() {
+  return typeof window !== 'undefined' && !!window.document?.createElement;
+}
